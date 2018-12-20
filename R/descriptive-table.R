79bc9f795de43add99184cc720aa206d6d6dc74a
aspina7 <alexfspina@gmail.com>
2018-12-20 07:55:41 +0000

    not beautiful but it works

--- R/descriptive-table.R
+++ R/descriptive-table.R
@@ -2,27 +2,27 @@
 #' @import tidyr
 #' @export
 
-descriptive <- function(df, variable, group = NA, multiplier = 100, digits = 1){
+descriptive <- function(df, counter, grouper = NA, multiplier = 100, digits = 1){
 
-  if (!is.na(group)) {
-    count_data <- group_by(df, df[,group]) %>% 
-                    count(df[, variable]) %>% 
-                    mutate(prop = round(n/sum(n)*multiplier, digits = digits) %>% 
-                    # gather(variable, value, c(n, prop)) %>% 
-                    # unite(temp, sex, variable, sep = "_") %>% 
-                    # spread(temp, value)
+  if (!is.na(grouper)) {
+    count_data <- group_by_(df, grouper) %>% 
+                    count_(counter) %>% 
+                    mutate(prop = round(n/sum(n)*multiplier, digits = digits))
+    
+    count_data <- count_data %>% 
+                    gather(variable, value, c(n, prop)) %>%
+                    unite(temp, !!as.name(grouper), variable, sep = "_") %>%
+                    spread(temp, value)
+    
   } else{
     # get counts
-    count_data <- count(df, df[,variable])
+    count_data <- count_(df, outcome)
     # get total 
     total <- sum(count_data$n)
     
     count_data <- count_data %>% 
                   rowwise() %>% 
                   mutate(prop = round(n / total*multiplier, digits = digits))
-    
-    # change var names
-    colnames(count_data) <- c(variable, "n", "prop")
 
   }
   
