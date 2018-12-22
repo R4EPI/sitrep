d0d45b5e483a5bbc70850af6c2ba757a549ab8fe
aspina7 <alexfspina@gmail.com>
2018-12-22 14:51:43 +0000

    Tried adding totals, but still needs fixing

--- R/descriptive-table.R
+++ R/descriptive-table.R
@@ -5,10 +5,13 @@
 #' @param grouper A name of the variable (in quotation marks) that you would like to have as columns. 
 #' @param multiplier What you would like to have your proportions as (default is per 100). 
 #' @param digits The number of decimal places you would like in your proportions (default is 1). 
+#' @param coltotals Add column totals on the end
+#' @param rowtotals Add row totals (only sums counts)
 #' @import tidyr
 #' @export
 
-descriptive <- function(df, counter, grouper = NA, multiplier = 100, digits = 1){
+descriptive <- function(df, counter, grouper = NA, multiplier = 100, digits = 1, 
+                        coltotals = FALSE, rowtotals = FALSE){
 
   # if given two variables then group by the "grouper" var
   if (!is.na(grouper)) {
@@ -32,6 +35,29 @@ descriptive <- function(df, counter, grouper = NA, multiplier = 100, digits = 1)
   
   # if there are NA counts, then change these to zero (except) in first col (which contains )
   count_data <- count_data %>%  mutate_at(2:ncol(.), funs(replace(., is.na(.), 0)))
+  
+  
+  # if (coltotals == TRUE) {
+  #   
+  #   #make available to dplyr
+  #   counter <- enquo(counter)
+  #   
+  #   count_data <- count_data %>%
+  #     # change first column (with var levels) in to a character (for rbinding)
+  #     ungroup() %>%
+  #     mutate(!!counter := as.character(!!counter)) %>%
+  #     # cbind a dataframe with "total and the column sums), rbind that to bottom
+  #     rbind(cbind(!!counter := "Total",
+  #                 t(round(colSums(count_data[,2:ncol(count_data)], na.rm = TRUE), digits = 0))))
+  # }
+
+  if (rowtotals == TRUE) {
+    count_data <- count_data %>%
+      # add columns which have "_n" in the name
+      mutate(Total = rowSums(count_data[,grep("_n", colnames(count_data))], na.rm = TRUE))
+  }
+
+  
   count_data
   
 }
