54d163f08a6f1ef68487c8099bd110a629949e48
aspina7 <alexfspina@gmail.com>
2019-01-07 23:49:11 +0000

    Used styler on package and this got changed automatically

--- tests/testthat/test-proportion.R
+++ tests/testthat/test-proportion.R
@@ -1,22 +1,20 @@
 context("proportion() tests")
 
 test_that("proportion() CI gets narrower with increasing sample size", {
-  p5   <- proportion(5, 10)
-  p50  <- proportion(50, 100)
+  p5 <- proportion(5, 10)
+  p50 <- proportion(50, 100)
   p500 <- proportion(500, 1000)
   # All are data frames ------
-  expect_is(p5,   "data.frame")
-  expect_is(p50,  "data.frame")
+  expect_is(p5, "data.frame")
+  expect_is(p50, "data.frame")
   expect_is(p500, "data.frame")
   # Proportions are equal ------
-  expect_identical(p5$prop,  p50$prop)
+  expect_identical(p5$prop, p50$prop)
   expect_identical(p50$prop, p500$prop)
   # Lower CI increases ------
   expect_lt(p5$lower, p50$lower)
   expect_lt(p50$lower, p500$lower)
-  # Upper CI decreases ------  
+  # Upper CI decreases ------
   expect_gt(p5$upper, p50$upper)
   expect_gt(p50$upper, p500$upper)
 })
-
-
