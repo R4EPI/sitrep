1bbdb2b900455666fe3c67bd5289ba0bc5f828af
Zhian N. Kamvar <zkamvar@gmail.com>
2019-01-08 18:31:27 +0900

    update descriptive by removing the pipe
    It still comes back with nse warnings, though :/

--- R/descriptive-table.R
+++ R/descriptive-table.R
@@ -7,37 +7,41 @@
 #' @param digits The number of decimal places you would like in your proportions (default is 1).
 #' @param coltotals Add column totals on the end
 #' @param rowtotals Add row totals (only sums counts)
-#' @import tidyr
+#' @importFrom dplyr group_by count mutate mutate_at
+#' @importFrom tidyr gather unite spread 
+#' @importFrom rlang sym "!!"
 #' @export
 
 descriptive <- function(df, counter, grouper = NA, multiplier = 100, digits = 1,
                         coltotals = FALSE, rowtotals = FALSE) {
   
+  # Using rlang::sym() allows us to use quoted arguments
+  # If we wanted to go full NSE, we would use rlang::enquo() instead
+  sym_count <- rlang::sym(counter)
   # if given two variables then group by the "grouper" var
   if (!is.na(grouper)) {
+    sym_group <- rlang::sym(grouper)
     # produce count table with props column-wise (seperate for each "grouper" level)
-    count_data <- group_by_(df, grouper) %>%
-      count_(counter) %>%
-      mutate(prop = round(n / sum(n) * multiplier, digits = digits))
+    count_data <- group_by(df, !!sym_group)
+    count_data <- count(count_data, !!sym_count)
+    count_data <- dplyr::mutate(count_data, 
+                                prop = round(n / sum(n) * multiplier, 
+                                             digits = digits))
 
     # change to wide format, to have "grouper" var levels as columns
-    count_data <- count_data %>%
-      gather(variable, value, c(n, prop)) %>%
-      unite(temp, !!grouper, variable, sep = "_") %>%
-      spread(temp, value)
+    count_data <- gather(count_data, variable, value, c(n, prop))
+    count_data <- unite(count_data, temp, !!sym_group, variable, sep = "_")
+    count_data <- spread(count_data, temp, value)
   } else {
-    # Using rlang::sym() allows us to use quoted arguments
-    # If we wanted to go full NSE, we would use rlang::enquo() instead
-    sym_count  <- rlang::sym(counter)
     # get counts and props for just a single variable
-    count_data <- count(df, !!sym_count)
-    count_data <- mutate(count_data, 
-                         prop = round(n / sum(n) * multiplier, 
-                                      digits = digits))
+    count_data <- dplyr::count(df, !!sym_count)
+    count_data <- dplyr::mutate(count_data, 
+                                prop = round(n / sum(n) * multiplier, 
+                                             digits = digits))
   }
 
   # if there are NA counts, then change these to zero (except) in first col (which contains )
-  count_data <- count_data %>% mutate_at(2:ncol(.), funs(replace(., is.na(.), 0)))
+  count_data <- mutate_at(count_data, 2:ncol(count_data), funs(replace(., is.na(.), 0)))
 
 
   # if (coltotals == TRUE) {
@@ -55,9 +59,10 @@ descriptive <- function(df, counter, grouper = NA, multiplier = 100, digits = 1,
   # }
 
   if (rowtotals == TRUE) {
-    count_data <- count_data %>%
+    count_data <- 
       # add columns which have "_n" in the name
-      mutate(Total = rowSums(count_data[, grep("_n", colnames(count_data))], na.rm = TRUE))
+      mutate(count_data, 
+             Total = rowSums(count_data[, grep("(_n$|^n$)", colnames(count_data))], na.rm = TRUE))
   }
 
 
