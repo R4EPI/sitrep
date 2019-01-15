aebe9e3d23da4069513dc47e809729e080125db9
Zhian N. Kamvar <zkamvar@gmail.com>
2019-01-15 17:37:02 +0900

    update y labels to be symmetric
    This ensures that the y labels are symmetric around zero.

--- R/age-pyramid.R
+++ R/age-pyramid.R
@@ -12,7 +12,7 @@
 #'
 #' set.seed(2018-01-15)
 #' ages <- cut(sample(80, 150, replace = TRUE),
-#'             breaks = c(0, 5, 10, 30, 50, 100), right = FALSE)
+#'             breaks = c(0, 5, 10, 30, 90), right = FALSE)
 #' sex  <- sample(c("Female", "Male"), 150, replace = TRUE)
 #' ill  <- sample(0:1, 150, replace = TRUE)
 #' dat  <- data.frame(AGE = ages, sex = sex, ill = ill, stringsAsFactors = FALSE)
@@ -31,7 +31,8 @@ plot_age_pyramid <- function(data, age_group = "age_group", split_by = "sex") {
   sex_levels <- unique(data[[split_by]])
   stopifnot(length(sex_levels) >= 1L, length(sex_levels) <= 2L)
   plot_data[["n"]] <- ifelse(plot_data[[split_by]] == sex_levels[[1L]], -1L, 1L) * plot_data[["n"]]
-  the_breaks <- sort(c(0, seq(-max_n, max_n, step_size)))
+  the_breaks <- seq(0, max_n, step_size)
+  the_breaks <- c(-rev(the_breaks[-1]), the_breaks)
   ggplot(plot_data) +
     aes(x = !!ag, y = !!quote(n), fill = !!sb) +
     geom_bar(stat = "identity") +
