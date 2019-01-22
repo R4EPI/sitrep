fadc61c13f60807ba40815d9dd350d7acec5e735
Zhian N. Kamvar <zkamvar@gmail.com>
2019-01-22 20:03:32 +0900

    Revert "use markdown documentation"
    This reverts commit 6f0643dda68d769ab2dce85ef380f07889f6ab7d.

--- tests/testthat/test-proportion.R
+++ tests/testthat/test-proportion.R
@@ -1,10 +1,9 @@
 context("proportion() tests")
 
-p5   <- attack_rate(5, 10)
-p50  <- case_fatality_rate(50, 100)
-p500 <- attack_rate(500, 1000)
-
-test_that("CI gets narrower with increasing sample size", {
+test_that("proportion() CI gets narrower with increasing sample size", {
+  p5 <- proportion(5, 10)
+  p50 <- proportion(50, 100)
+  p500 <- proportion(500, 1000)
   # All are data frames ------
   expect_is(p5, "data.frame")
   expect_is(p50, "data.frame")
@@ -18,18 +17,4 @@ test_that("CI gets narrower with increasing sample size", {
   # Upper CI decreases ------
   expect_gt(p5$upper, p50$upper)
   expect_gt(p50$upper, p500$upper)
-})
-
-test_that("numbers are not rounded", {
-  expect_equal(p5$lower, 23.659309, tol = 1e-6)
-  expect_equal(p50$lower, 40.383153, tol = 1e-6)
-})
-
-test_that("mortality rates work", {
-  # see https://www.cdc.gov/ophss/csels/dsepd/ss1978/lesson3/section3.html
-  accidentals   <- 106742
-  US_population <- 288357000
-  mr <- mortality_rate(accidentals, US_population, multiplier = 10^5)
-  expect_named(mr, c("deaths", "population", "mortality per 100 000", "lower", "upper"))
-  expect_equal(mr$"mortality per 100 000", 37.02, tol = 0.01)
 })
