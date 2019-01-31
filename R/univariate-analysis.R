e6120d1f5357ece7d91c5fd7a497e82e572e2311
aspina7 <alexfspina@gmail.com>
2019-01-31 18:14:59 +0000

    added options verbose(drops a few columns) and mergeCI(puts CIs in one column)

--- R/univariate-analysis.R
+++ R/univariate-analysis.R
@@ -1,13 +1,18 @@
 #' Produce risk ratios or odds ratios
 #' @param measure Specify what you would like to calculated, options are "OR" or "RR"
+#' default is "OR"
 #' @param digits Specify number of decimal places
+#' @param verbose Specify whether would like all outputs in table (default is TRUE)
+#' Non-verbose output drops group odds/risk calculations as well as p-value
+#' @param mergeCI Whether or not to put the confidence intervals in one column (default is FALSE)
 #' @param outcome Vector of TRUE/FALSE from your outcome variable of interest
 #' @param ... TRUE/FALSE vectors for the exposure variables of interest
 #' @references Inspired by Daniel Gardiner,
 #' see [github repo](https://github.com/DanielGardiner/UsefulFunctions/blob/efffde624d424d977651ed1a9ee4430cbf2b0d6f/single.variable.analysis.v0.3.R#L12)
 #' @export
 
-univariate_analysis <- function(measure = "OR", digits = 3, outcome, ...) {
+univariate_analysis <- function(measure = "OR", digits = 3, verbose = TRUE,
+                                mergeCI = FALSE, outcome, ...) {
   n <- length(outcome)
   predictors <- list(...)
   predictor_labels <- substitute(list(...))
@@ -34,7 +39,7 @@ univariate_analysis <- function(measure = "OR", digits = 3, outcome, ...) {
       or <- epitools::oddsratio(table, method = "wald")
       midp_pval_or <- round(or$p.value[2L, 1L], digits = digits)
 
-      cbind(
+      outtie <- cbind(
         data.frame(
           exp_cases = or$data[2L, 2L],
           unexp_cases = or$data[1L, 2L],
@@ -46,6 +51,20 @@ univariate_analysis <- function(measure = "OR", digits = 3, outcome, ...) {
         t(round(or$measure[2L, ], digits = digits)),
         data.frame(p_value = midp_pval_or)
       )
+
+      if (verbose == FALSE) {
+        outtie$cases_odds <- NULL
+        outtie$noncases_odds <- NULL
+        outtie$p_value <- NULL
+      }
+
+      if (mergeCI == TRUE) {
+        outtie$ci <- paste0(outtie$lower, " - ", outtie$upper)
+        outtie$lower <- NULL
+        outtie$upper <- NULL
+      }
+
+      outtie
     })
   }
 
@@ -59,7 +78,7 @@ univariate_analysis <- function(measure = "OR", digits = 3, outcome, ...) {
       rr <- epitools::riskratio(table, method = "wald")
       midp_pval_rr <- round(rr$p.value[2L, 1L], digits = digits)
 
-      cbind(
+      outtie <- cbind(
         data.frame(
           exp_cases = rr$data[2L, 2L],
           exp = rr$data[2L, 3L],
@@ -71,6 +90,19 @@ univariate_analysis <- function(measure = "OR", digits = 3, outcome, ...) {
         t(round(rr$measure[2L, ], digits = digits)),
         data.frame(p_value = midp_pval_rr)
       )
+
+      if (verbose == FALSE) {
+        outtie$exp_AR <- NULL
+        outtie$unexp_AR <- NULL
+        outtie$p_value <- NULL
+      }
+
+      if (mergeCI == TRUE) {
+        outtie$ci <- paste0(outtie$lower, " - ", outtie$upper)
+        outtie$lower <- NULL
+        outtie$upper <- NULL
+      }
+      outtie
     })
   }
 
