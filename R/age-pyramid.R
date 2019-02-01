db826211e79d659d9c67b99ed66d59994b959730
Zhian N. Kamvar <zkamvar@gmail.com>
2019-02-01 20:34:44 +0900

    add stack_by; remove left line; add dashes

--- R/age-pyramid.R
+++ R/age-pyramid.R
@@ -1,5 +1,5 @@
 #' Plot a population pyramid (age-sex) from a dataframe.
-#' This assumes that your variables are called "age_group" and "sex", respectively.
+#'
 #' @param data Your dataframe (e.g. linelist)
 #' @param age_group the name of a column in the data frame that defines the age
 #'   group categories. Defaults to "age_group"
@@ -40,27 +40,31 @@ plot_age_pyramid <- function(data, age_group = "age_group", split_by = "sex", st
   }
   ag <- rlang::sym(age_group)
   sb <- rlang::sym(split_by)
+  st <- rlang::sym(stack_by)
   plot_data <- tidyr::complete(data, !!ag) # make sure all factors are represented
-  plot_data <- dplyr::group_by(plot_data, !!ag, !!sb)
+  plot_data <- dplyr::group_by(plot_data, !!ag, !!sb, !!st)
   plot_data <- dplyr::summarise(plot_data, n = dplyr::n())
   max_n <- signif(max(plot_data[["n"]]), digits = -1)
   stopifnot(is.finite(max_n), max_n > 0)
   step_size <- ceiling(max_n / 5)
   sex_levels <- unique(data[[split_by]])
+  stk_levels <- unique(data[[stack_by]])
   stopifnot(length(sex_levels) >= 1L, length(sex_levels) <= 2L)
   plot_data[["n"]] <- ifelse(plot_data[[split_by]] == sex_levels[[1L]], -1L, 1L) * plot_data[["n"]]
   the_breaks <- seq(0, max_n, step_size)
   the_breaks <- c(-rev(the_breaks[-1]), the_breaks)
   pyramid <- ggplot(plot_data) +
-    aes(x = !!ag, y = !!quote(n), fill = !!sb) +
+    aes(x = !!ag, y = !!quote(n), group = !!sb, fill = !!st) +
     geom_bar(stat = "identity") +
-    geom_hline(xintercept = 0) +
+    geom_hline(yintercept = 0) + # add vertical line 
     coord_flip() +
-    scale_fill_manual(values = incidence::incidence_pal1(length(sex_levels))) +
+    scale_fill_manual(values = incidence::incidence_pal1(length(stk_levels))) +
     scale_y_continuous(limits = c(-max_n, max_n),
                        breaks = the_breaks,
                        labels = abs(the_breaks)) +
-    theme_classic() 
+    theme_classic() +
+    theme(axis.line.y.left = element_blank()) +
+    theme(panel.grid.major.y = element_line(linetype = 2))
 
   if (vertical_lines == TRUE) {
     pyramid <- pyramid +
