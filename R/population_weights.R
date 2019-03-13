ec6390eb8140bcdaaa88add4f66aa2ba52d8e246
aspina7 <alexfspina@gmail.com>
2019-03-13 11:40:00 +0000

    update add_weights to remove "name = study" argument in counts

--- R/population_weights.R
+++ R/population_weights.R
@@ -19,23 +19,23 @@ add_weights <- function(x, p, ..., population = population, weight = weight, wei
   weight_ID  <- rlang::enquo(weight_ID)
   weight     <- rlang::enquo(weight)
 
-  # create a merger ID by age group and sex 
+  # create a merger ID by age group and sex
   p <- tidyr::unite(p, "ID", !!! .dots)
 
-  # get study sample counts 
-  counts <- dplyr::count(x, !!! .dots, name = "study")
+  # get study sample counts
+  counts <- dplyr::count(x, !!! .dots)
   counts <- tidyr::unite(counts, "ID", !!! .dots)
-  counts <- dplyr::select(counts, .data$ID, .data$study)
+  counts <- dplyr::select(counts, .data$ID, .data$n)
 
-  # merge counts to population data 
+  # merge counts to population data
   p <- dplyr::left_join(p, counts, by = "ID")
 
-  # create weight variable 
-  p <- dplyr::mutate(p, !! weight := !! population / .data$study) 
+  # create weight variable
+  p <- dplyr::mutate(p, !! weight := !! population / .data$n)
   p <- dplyr::select(p, .data$ID, !! weight)
 
 
-  # merge to study sample 
+  # merge to study sample
   merge_by <- "ID"
   names(merge_by) <- as.character(rlang::expr(weight_ID))
   d <- tidyr::unite(x, !! weight_ID, !!! .dots, remove = FALSE)
