5d56e84758f92838e7d7c0b893267bb2e3286412
Zhian N. Kamvar <zkamvar@gmail.com>
2019-05-02 17:17:25 +0100

    fix template_data_frame for MULTI variables

--- R/template_data_frame.R
+++ R/template_data_frame.R
@@ -21,15 +21,15 @@ template_data_frame_categories <- function(dat_dict, numcases, varnames, survey 
     dis_output[[i]] <- sample(vals, numcases, replace = TRUE)
   }
 
-  # multivars <- dat_dict[dat_dict$data_element_valuetype == "MULTI", varnames]
+  multivars <- dat_dict[[varnames]][dat_dict$data_element_valuetype == "MULTI"]
 
-  # if (length(multivars) > 0) {
-  #   sample_multivars <- lapply(multivars, sample_cats)
-  #   sample_multivars <- do.call(cbind, sample_multivars)
+  if (length(multivars) > 0) {
+    sample_multivars <- lapply(multivars, sample_cats, numcases = numcases, 
+                               df = categories, varnames = varnames)
 
-  #   dis_output[, multivars] <- NULL
-  #   dis_output <- cbind(dis_output, sample_multivars)
-  # }
+    dis_output[, multivars] <- NULL
+    dis_output <- dplyr::bind_cols(dis_output, sample_multivars)
+  }
 
   dis_output
 }
@@ -42,13 +42,17 @@ sample_single <- function(x, size, prob = 0.1) {
 
 
 # random data for one single "MULTI" variable (split into multiple columns)
-sample_cats <- function(categories, cat, numcases) {
-  lvls <- as.character(categories[[cat]])
+sample_cats <- function(category, numcases, df, varnames) {
+
+  dat <- df[df[[varnames]] == category, , drop = FALSE]
+
+  lvls <- dat$option_name
   # define suffixes for column names, e.g. 000, 001, 002, ...
-  suffixes <- formatC((1:length(lvls)) - 1, width = 3, format = "d", flag = "0")
+  suffixes <- formatC((seq_along(lvls)) - 1, width = 3, format = "d", flag = "0")
 
   # create columns with randomized lvls with randomized probability
-  extra_cols <- sapply(lvls, sample_single, size = numcases, prob = sample(5:15, 1) / 100)
-  colnames(extra_cols) <- paste0(cat, "_", suffixes)
-  extra_cols
+  extra_cols <- vapply(lvls, sample_single, FUN.VALUE = character(numcases), 
+                       size = numcases, prob = sample(5:15, 1) / 100)
+  colnames(extra_cols) <- paste0(category, "_", suffixes)
+  as.data.frame(extra_cols, stringsAsFactors = FALSE)
 }
