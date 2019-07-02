92d1b3a7f1a797315b463acc34d5349a52bcb14a
Zhian N. Kamvar <zkamvar@gmail.com>
2019-07-02 16:37:38 +0100

    update zcurve to take a data frame

--- R/zcurve.R
+++ R/zcurve.R
@@ -1,21 +1,37 @@
 #' Create a curve comparing observed Z-scores to the WHO standard.
 #'
-#' @param zscore a umeric vector containing computed zscores
+#' @param dat a data frame
+#' @param zscore bare name of a numeric vector containing computed zscores
 #' @return a ggplot2 object that is customisable via the ggplot2 package.
 #' @export
 #' @importFrom ggplot2 ggplot aes stat_function geom_density scale_x_continuous labs
 #' @examples
 #' library("ggplot2")
-#' dat <- rnorm(204) + runif(1) # slightly skewed
-#' zcurve(dat) +
+#' set.seed(9)
+#' dat <- data.frame(observed = rnorm(204) + runif(1),
+#'                   skewed   = rnorm(204) + runif(1, 0.5)
+#'                  ) # slightly skewed
+#' zcurve(dat, observed) +
 #'   labs(title = "Weight-for-Height Z-scores") +
 #'   theme_classic()
-zcurve <- function(zscore) {
+#'
+#' zcurve(dat, skewed) +
+#'   labs(title = "Weight-for-Height Z-scores") +
+#'   theme_classic()
+zcurve <- function(x, zscore) {
 
-  stopifnot(is.numeric(zscore))
-  dat <- data.frame(observed = zscore)
+  if (!is.data.frame(x)) {
+    stop("x must be a data frame")
+  }
+
+  zsc <- tidyselect::vars_pull(names(x), !! rlang::enquo(zscore))
+
+  if (!is.numeric(x[[zsc]])) {
+    stop("zscore must be a numeric variable")
+  }
+
   ggplot(dat) +
-    geom_density(aes(x = !!quote(observed), color = "observed")) +
+    stat_density(aes(x = !! rlang::enquo(zscore), color = "observed"), geom = "line") +
     stat_function(fun = stats::dnorm, 
                   args = list(mean = 0, sd = 1),
                   mapping = aes(color = "WHO standard")
@@ -27,7 +43,7 @@ zcurve <- function(zscore) {
     scale_x_continuous(limits = c(-6, 6)) + 
     labs(
          x = "Z-score",
-         y = sprintf("%% of cases\n(n = %d)", length(zscore))
+         y = sprintf("%% of cases\n(n = %d)", nrow(x))
     )
   
 }
