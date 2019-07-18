ce4f239636809e3ebd6e6bfb601e0e86ae0ac319
aspina7 <alexfspina@gmail.com>
2019-07-18 09:12:31 +0100

    zcurve changing labels and line width

--- R/zcurve.R
+++ R/zcurve.R
@@ -4,7 +4,8 @@
 #' @param zscore bare name of a numeric vector containing computed zscores
 #' @return a ggplot2 object that is customisable via the ggplot2 package.
 #' @export
-#' @importFrom ggplot2 ggplot aes stat_function geom_density scale_x_continuous labs
+#' @importFrom ggplot2 ggplot aes stat_function geom_density scale_x_continuous scale_y_continuous labs
+#' scales percent_format
 #' @examples
 #' library("ggplot2")
 #' set.seed(9)
@@ -31,20 +32,21 @@ zcurve <- function(x, zscore) {
   }
 
   ggplot(x) +
-    stat_density(aes(x = !! rlang::enquo(zscore), color = "observed"), 
+    stat_density(aes(x = !! rlang::enquo(zscore), color = "Observed"), size = 1,
                  geom = "line") +
     stat_function(fun     = stats::dnorm,
-                  args    = list(mean = 0, sd = 1),
+                  args    = list(mean = 0, sd = 1), size = 1,
                   mapping = aes(color = "WHO standard")
                  ) +
     scale_color_manual("",
-                       values = c("observed" = "red",
-                                  "WHO standard" = "black") 
+                       values = c("Observed" = "red",
+                                  "WHO standard" = "dark green")
     ) +
-    scale_x_continuous(limits = c(-6, 6)) + 
+    scale_x_continuous(limits = c(-6, 6)) +
+    scale_y_continuous(labels = scales::percent_format()) +
     labs(
          x = "Z-score",
-         y = sprintf("%% of cases\n(n = %d)", nrow(x))
+         y = sprintf("Proportion of children\n(n = %d)", nrow(x))
     )
-  
+
 }
