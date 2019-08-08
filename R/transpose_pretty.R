43628731bc0e883492988713fa8aa83d7f22f0e2
Zhian N. Kamvar <zkamvar@gmail.com>
2019-08-08 11:32:37 +0100

    move flipper to the transpose file; add docs

--- R/transpose_pretty.R
+++ R/transpose_pretty.R
@@ -29,3 +29,72 @@ transpose_pretty <- function(x, columns, rows, suffix, clev = NULL) {
   names(res)[-1] <- paste0(names(res)[-1], sfx_char)
   res
 }
+
+#' flip multiple columns of a table
+#'
+#' @param x survey data (TODO: CHANGE THIS)
+#' @param res the resulting table with things to flip
+#' @param transpose transpose by which columns
+#' @param pretty is the table already pretty (gives ci or proportion columns)
+#' @param stra the quoted strata variable (TODO: CHANGE THIS)
+#'
+#' @return a flipped data frame where the stratifying variables are flipped
+#' @noRd
+#'
+#' @examples
+#' 
+flipper <- function(x, res, transpose = c("variable", "value", "both"), pretty = TRUE, stra) {
+
+  transpose <- match.arg(tolower(transpose), c("variable", "value", "both"))
+  if (transpose == "both") {
+    # if the user wants to keep both columns, then we unite them and then
+    res <- tidyr::unite(res, col = "both", "variable", "value", remove = TRUE)
+  }
+
+  # number of rows in the original table
+  nr   <- seq_len(nrow(res))
+  # if deff exists in the table
+  deff <- any(grepl("deff$", names(res)))
+  # number of new columns based on the number of rows
+  nc   <- 1L + deff + if (pretty) 1L else 3L
+  # the variable column to be transposed
+  var  <- rlang::ensym(transpose)
+
+  # when there is no strata, we have to come up with a dummy variable
+  strata_exists <- tidyselect::vars_select(colnames(x), !! stra)
+  strata_exists <- length(strata_exists) > 0
+
+  if (strata_exists) {
+    slevels <- dplyr::pull(x$variables, !! stra)
+    slevels <- if (is.factor(slevels)) levels(slevels) else sort(slevels)
+  } else {
+    stra      <- paste0("__", as.integer(Sys.time()))
+    stra      <- rlang::ensym(stra)
+    old_names <- names(res)[names(res) != transpose]
+    names(res)[names(res) != transpose] <- paste0(" ", old_names)
+    slevels   <- NULL
+  }
+
+  # transposing the count variable, which is always there.
+  tres <- transpose_pretty(res, !! stra, !! var, !! rlang::sym("n"), slevels)
+
+  # determining the list of suffixes to run through when appending the columns
+  suffix <- c(
+              if (deff) "deff" else NULL,
+              if (pretty) "ci" else c("proportion", "proportion_low", "proportion_upp")
+  )
+
+  # transposing and appending the columns
+  for (i in suffix) {
+    suff <- rlang::ensym(i)
+    tmp  <- transpose_pretty(res, !! stra, !! var, !! suff, slevels)
+    tres <- dplyr::bind_cols(tres, tmp[-1])
+  }
+
+  # re-ordering the columns so that they are grouped by the original row order
+  res <- tres[c(1, order(rep(nr, nc)) + 1)]
+  if (!strata_exists) {
+    res <- dplyr::select(res, - !! stra)
+  }
+  res
+}
