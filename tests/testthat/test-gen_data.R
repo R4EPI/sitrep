767254c5a3a50a342f625ab2adbbe10bb050d846
Zhian N. Kamvar <zkamvar@gmail.com>
2019-09-13 15:11:56 +0100

    add rudimentary tests for gen_data

--- tests/testthat/test-gen_data.R
+++ tests/testthat/test-gen_data.R
@@ -1,6 +1,8 @@
+
+outbreaks <- c("Measles", "Cholera", "AJS", "Meningitis")
+
 test_that("msf_dict works", {
 
-  outbreaks <- c("Measles", "Cholera", "AJS", "Meningitis")
   for (disease in c(outbreaks)) {
     nested  <- msf_dict(disease)
     long    <- msf_dict(disease, compact = FALSE)
@@ -19,4 +21,13 @@ test_that("msf_dict works", {
 
 })
 
+test_that("data can be generated", {
 
+  for (disease in c(outbreaks)) {
+    dictionary <- msf_dict(disease)
+    data       <- gen_data(disease, numcases = 30)
+
+    expect_is(data, "tbl_df", label = disease)
+    expect_equal(nrow(dictionary), ncol(data), label = disease)
+  }
+})
