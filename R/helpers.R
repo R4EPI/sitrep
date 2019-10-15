937c72e4bc384b4f34250028133bbe377e0893dd
Zhian N. Kamvar <zkamvar@gmail.com>
2019-10-15 10:12:44 +0100

    unexport unused code

--- R/helpers.R
+++ R/helpers.R
@@ -1,20 +1,3 @@
-# Helper function to splice a data.frame with multiple columns
-# into a dplyr function
-# example
-# dplyr::summarise(mtcars,
-# n = n(),
-# x = sum(hp > 100),
-# !!!splice_df(binom::binom.wilson(x, n), mean, lower, upper)
-# )
-splice_df <- function(x, ...) {
-  expr <- rlang::enquo(x)
-  cols <- lapply(rlang::ensyms(..., .named = TRUE), as.character)
-  lapply(cols, function(col_name) {
-    rlang::quo(`[[`(!!expr, !!col_name))
-  })
-}
-
-
 # This will convert numbers to factors.
 #
 # If the number of unique numbers is five or fewer, then they will simply
