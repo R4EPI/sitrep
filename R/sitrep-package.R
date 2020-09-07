67ef0c905d73b2d01d380ad5dcf96bbe574a77d3
Zhian N. Kamvar <zkamvar@gmail.com>
2020-09-07 13:25:00 -0700

    switch to epibuffet; remove outbreaks; add gha

---
+++ R/sitrep-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom glue glue
+#' @importFrom tibble as_tibble
+#' @importFrom stats setNames
+#' @importFrom rlang .data := !!
+#' @importFrom dplyr count
+#' @importFrom ggplot2 ggplot aes stat_function geom_density scale_x_continuous scale_y_continuous labs expand_scale stat_density scale_color_manual
+#' @importFrom scales percent_format
+#' @importFrom apyramid age_pyramid
+## usethis namespace: end
+NULL
