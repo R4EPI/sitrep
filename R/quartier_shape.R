ab408a4af0d1e51b940e3da502ba2f25d1031f51
Neale Batra <52152191+nsbatra@users.noreply.github.com>
2022-08-26 10:52:26 +0200

    add 4 datasets to data and extdata

---
+++ R/quartier_shape.R
@@ -0,0 +1,28 @@
+#' Quartier shapefile for example outbreak Hepatatis E virus or Acute Jaundice Syndrome (AJS)
+#'
+#' Shapefile for quartier (polygons) from a real outbreak of Hepatitis E virus (HEV) infection which
+#' occurred in the Chadian town of Am Timan between October 2016 and
+#' April 2017. The Chadian Ministry of Health (MoH) has approved the use
+#'  of this data for training purposes.
+#'  Please note, that some data has been adapted in order to best achieve
+#'  training objectives. Also, the GPS coordinates included in the dataset
+#'  do not correspond to real cases identified during this outbreak.
+#'  They have been generated exclusively for training purposes.
+#'
+#' @docType data
+#'
+#' @usage data(quartier_shape)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references None
+#' (\href{https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0188240}{PLoS})
+#'
+#' @source \href{https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0188240}{Article}
+#'
+#' @examples
+#' data(quartier_shape)
+#' head(quartier_shape)
+"quartier_shape"
