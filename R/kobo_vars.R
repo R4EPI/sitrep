db5c48d7bd377cd8457e97e291bcf892db31d24c
Neale Batra <52152191+nsbatra@users.noreply.github.com>
2022-08-30 11:57:11 +0200

    add 4 survey datasets

---
+++ R/kobo_vars.R
@@ -0,0 +1,21 @@
+#' Example Kobo data dictionary, showing variables
+#'
+#' This is an example of a Kobo data dictionary (the sheet showing information on the variables)
+#'
+#' @docType data
+#'
+#' @usage data(kobo_vars)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references None
+#' (\href{https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0188240}{PLoS})
+#'
+#' @source \href{https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0188240}{Article}
+#'
+#' @examples
+#' data(kobo_vars)
+#' head(kobo_vars)
+"kobo_vars"
---
+++ R/survey_data.R
@@ -0,0 +1,21 @@
+#' Example data from a retrospective mortality survey (linelist)
+#'
+#' This is an example of data from a retrospective mortality survey
+#'
+#' @docType data
+#'
+#' @usage data(survey_data)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references None
+#' (\href{https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0188240}{PLoS})
+#'
+#' @source \href{https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0188240}{Article}
+#'
+#' @examples
+#' data(survey_data)
+#' head(survey_data)
+"survey_data"
